8dc4e71837b40559d5bb09e4dbcecc15f11570cf
josduaper <josduaper@alu.edu.gva.es>
2025-02-06 13:36:26 +0100

    libreria

--- src/Ejercicio7/Historico.java
+++ src/Ejercicio7/Historico.java
@@ -1,8 +1,47 @@
 package Ejercicio7;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Historico {
     private String fechaPrestamo;
     private String fechaDevuelta;
-    private Ejemplar ejemplar;
-    private Lector lector;
+    private List<Ejemplar> ejemplares;
+    private List<Lector> lectores;
+
+    public Historico(String fechaPrestamo, String fechaDevuelta) {
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevuelta = fechaDevuelta;
+        this.ejemplares = new ArrayList<>();
+        this.lectores = new ArrayList<>();
+    }
+
+    public String getFechaPrestamo() {
+        return this.fechaPrestamo;
+    }
+
+    public String getFechaDevuelta() {
+        return this.fechaDevuelta;
+    }
+
+    public List<Ejemplar> getEjemplar() {
+        return this.ejemplares;
+    }
+
+    public void addEjemplar(Ejemplar ejemplar) {
+        ejemplares.add(ejemplar);
+    }
+
+    public List<Lector> getLector() {
+        return this.lectores;
+    }
+
+    public void addLector(Lector lector) {
+        lectores.add(lector);
+    }
+
+    @Override
+    public String toString() {
+        return "Fecha prestamo: " + this.fechaPrestamo + ", Fecha devuelta: " + this.fechaDevuelta;
+    }
 }
